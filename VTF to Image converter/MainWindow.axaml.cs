c6c86a632a1937e55f6ff878f7290e24be5670ac
Kreedyy <kreedyhaj@gmail.com>
2025-09-04 16:13:39 +0200

    Add dropdown and resize button

--- VTF to Image converter/MainWindow.axaml.cs
+++ VTF to Image converter/MainWindow.axaml.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ImageSixLabor = SixLabors.ImageSharp.Image;
+using System.Diagnostics;
 
 namespace VTF_to_Image_converter
 {
@@ -33,14 +34,67 @@ namespace VTF_to_Image_converter
             foreach (var filePath in filePaths)
             {
                 var stream = File.OpenRead(filePath);
+                switch (filePath)
+                {
+                    case string s when IsFileImage(s):
+                        ImageToVTF(stream, filePath);
+                        break;
 
-                VtfFile vtfFile = new VtfFile(stream);
-                ImageSixLabor image = vtfFile.ToImage<Rgba32>();
-                stream.Close();
-                int length = filePath.Length;
-                string[] imageFilePath = filePath.Split(".");
-                image.SaveAsPng(imageFilePath[0] + ".png");
+                    case string s when s.EndsWith(".vtf"):
+                        VTFtoPNG(stream, filePath);
+                        break;
+
+                    default:
+                        Debug.WriteLine("File is not image");
+                        break;
+                }
             }
+        }
+        private VtfImageFormat GetSelectedImageFormatForVTF() //Read what is selected from a dropdown menu, default is RGBA8888
+        {
+            var format = VtfImageFormat.Dxt5;
+            return format;
+        }
+        private bool GetAutoResizeForVTF() //Read if checkbox is checked, resize to power of 2
+        {
+            return false;
+        }
+        private void VTFtoPNG(FileStream stream, string filePath)
+        {
+            VtfFile vtfFile = new VtfFile(stream);
+            ImageSixLabor image = vtfFile.ToImage<Rgba32>();
+            stream.Close();
+            int length = filePath.Length;
+            string[] imageFilePath = filePath.Split(".");
+            image.SaveAsPng(imageFilePath[0] + ".png");
+        }
+        private void ImageToVTF(FileStream stream, string filePath)
+        {
+            VtfFile vtfFile = new VtfFile();
+            ImageSixLabor image = ImageSixLabor.Load(stream);
+            image.ToVtfFile();
+
+
+            stream.Close();
+            int length = filePath.Length;
+            string[] imageFilePath = filePath.Split(".");
+            using (var output = File.Create(imageFilePath[0] + ".vtf"))
+            {
+                vtfFile.Write(output);
+            }
+        }
+        private bool IsFileImage(string filepath)
+        {
+            if (filepath.EndsWith(".png") || 
+                filepath.EndsWith(".jpg") || 
+                filepath.EndsWith(".jpeg") || 
+                filepath.EndsWith(".gif") || 
+                filepath.EndsWith(".bmp") || 
+                filepath.EndsWith(".webp"))
+            {
+                return true;
+            }
+            return false;
         }
 
         private async Task<string[]> GetSelectedFilePathsAsync()
